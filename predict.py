b8858edf4c23c0ea4de6e4fd89096b495cb4f41f
Michaela Denisová <michaeladenisova@gmail.com>
2023-06-01 19:25:32 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 ## Pre-trained model testing on new data
 # Importing
 
@@ -11,6 +8,8 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import argparse
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 
 
 def NormalizeData(df, column_name):
@@ -22,15 +21,8 @@ def NormalizeData(df, column_name):
     return df
 
 
-def process_test_data(test_df, precision, src_lng, tgt_lng):
-    if precision == 1:
-        test_df = test_df[test_df['position'] == 0]
-        test_df = test_df.rename(columns={'position': 'scaled_position'})
-    if precision == 5:
-        test_df = test_df[test_df['position'] < 5]
-        test_df = NormalizeData(test_df, "position")
-    if precision == 10:
-        test_df = NormalizeData(test_df, "position")
+def process_test_data(test_df,src_lng, tgt_lng):
+    test_df = NormalizeData(test_df, "position")
     test_df = NormalizeData(test_df, "freqs")
     test_df = NormalizeData(test_df, "index")
     test_data = []
@@ -67,21 +59,34 @@ def make_prediction(model, test_data, test_labels, test_df, word_pairs):
     predicted_output_data = model.predict(test_data)
     binary_predictions = np.round(predicted_output_data)
     test_df['predictions'] = binary_predictions 
+
     for index, value in enumerate(binary_predictions):
         print("Word pair:", word_pairs[index], "Label: ", test_labels[index], "Prediction: ", value)
+
+
+    tn, fp, fn, tp = confusion_matrix(test_labels, np.round(model.predict(test_data))).ravel()
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
     correct_predictions = np.sum(binary_predictions == test_labels)
     accuracy = correct_predictions / len(test_labels)
+
+    f1 = f1_score(test_labels, binary_predictions)
+
     print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1 score: ", f1)
     return test_df
 
 
-def main(precision, src_lng, tgt_lng, model_path, test_path, output):
+def main(src_lng, tgt_lng, model_path, test_path, output):
     print("Loading test data.")
     test_df = pd.read_csv(test_path)
     print("Loading the model.")
     model = load_model(model_path)
     print("Processing test data.")
-    test_data, test_labels, test_df, word_pairs = process_test_data(test_df, precision, src_lng, tgt_lng)
+    test_data, test_labels, test_df, word_pairs = process_test_data(test_df, src_lng, tgt_lng)
     print("Making predictions...")
     test_df = make_prediction(model, test_data, test_labels, test_df, word_pairs)
     ("Saving dataframe.")
@@ -91,7 +96,6 @@ def main(precision, src_lng, tgt_lng, model_path, test_path, output):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Model Training and Evaluation")
-    parser.add_argument("--precision", type=int, choices=[1, 5, 10], help="Select precision: 1, 5, or 10")
     parser.add_argument("--src_lng", type=str, help="Language code of the source language")
     parser.add_argument("--tgt_lng", type=str, help="Language code of the target language")
     parser.add_argument("--model_path", type=str, help="Path to pre-trained model")
@@ -100,5 +104,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.precision, args.src_lng, args.tgt_lng, args.model_path, args.test_path, args.output)
+    main(args.src_lng, args.tgt_lng, args.model_path, args.test_path, args.output)
 
